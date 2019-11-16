656ed2827c105fbe4dba166704e716ac16a22e93
igorsouzacarvalho88 <igmalte2@gmail.com>
2019-11-16 18:52:43 -0300

    Ajuste no factoryMethod e no abstractfactory número2

--- src/ufba/factorymethod/Main.java
+++ src/ufba/factorymethod/Main.java
@@ -13,14 +13,14 @@ public class Main {
 
         criadorDocumentacaoMarkdown criadorMarkdown = new criadorDocumentacaoMarkdown();
 
-        Cabecalho cabecalho = criadorMarkdown.geraCabecalho();
-        Corpo corpo = criadorMarkdown.geraCorpo();
-        Rodape rodape = criadorMarkdown.geraRodape();
+        Cabecalho cabecalho1 = criadorMarkdown.geraCabecalho();
+        Corpo corpo1 = criadorMarkdown.geraCorpo();
+        Rodape rodape1 = criadorMarkdown.geraRodape();
 
-        cabecalho.setTitulo("Padroes de projeto");
-        cabecalho.setEmpresa("Universidade Federal da Bahia");
-        corpo.setTexto("Aula de padroes de projeto na disciplina Engenharia de Software I.");
-        rodape.setData(new Date());
+        cabecalho1.setTitulo("Padroes de projeto");
+        cabecalho1.setEmpresa("Universidade Federal da Bahia");
+        corpo1.setTexto("Aula de padroes de projeto na disciplina Engenharia de Software I.");
+        rodape1.setData(new Date());
 
         // ----------------------------------------------------------------------------------
         criadorDocumentacao criador = new criadorDocumentacaoHTML();
