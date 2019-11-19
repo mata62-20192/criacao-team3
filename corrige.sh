a34da5429b3ff87101d7c3700b94ddb6b056c97d
Allan <allan.thales@gmail.com>
2019-11-19 19:53:54 -0300

    Tested

--- corrige.sh
+++ corrige.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Baixe o pattern4.jar a partir do seguinte site:
-#   https://users.encs.concordia.ca/~nikolaos/pattern_detection.html
+#   
 # Link direto para download:
 #  https://users.encs.concordia.ca/~nikolaos/files/pattern_detection/pattern4.jar
 #
 # Se a saída for 1 1, seu programa contém ambos os padrões solicitados
 #
 
-java -jar /opt/pattern4.jar -target bin -output out.xml
+java -jar pattern4.jar -target bin -output out.xml
 
 temFactoryMethod=0
 temAbstractFactory=0
