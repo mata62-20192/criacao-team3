727b31b2d92ead7c092e3c1382e6c316c5c27486
Allan <allan.thales@gmail.com>
2019-11-19 19:48:17 -0300

    Everything is fine

--- src/ufba/factorymethod/criadorDocumentacaoMarkdown.java
+++ src/ufba/factorymethod/criadorDocumentacaoMarkdown.java
@@ -6,6 +6,9 @@ import ufba.elementos.Rodape;
 import ufba.elementos.html.CabecalhoHTML;
 import ufba.elementos.html.CorpoHTML;
 import ufba.elementos.html.RodapeHTML;
+import ufba.elementos.markdown.CabecalhoMarkdown;
+import ufba.elementos.markdown.CorpoMarkdown;
+import ufba.elementos.markdown.RodapeMarkdown;
 
 public class criadorDocumentacaoMarkdown extends criadorDocumentacao {
     public criadorDocumentacaoMarkdown() {
